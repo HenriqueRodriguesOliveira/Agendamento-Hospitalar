86969b44bbcfdd1cc625b8d60516b2522900b74a
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2022-11-07 22:59:34 -0300

    tela de inicio e agendamento concluida

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,97 +1,81 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+import {
+  Background,
+  ContainerBar,
+  BotaoBanner,
+  Image,
+  Nome, 
+  Banner, Pesquisa, 
+  Lupa, Search, 
+  ContainerCard, Card, TituloCard, RowTitulo, Servico, STitulo, SubTitulo} from './style';
 
 export default function Home() {
+
+  const navigation = useNavigation();
+
  return (
-   <View style={estilo.container}>
-    <View style={estilo.containerRow}>
-    <Image source={require('../../assets/images/nuvem.png')} style={estilo.nuvem}/>
-    <Image source={require('../../assets/images/nuvem.png')} style={estilo.nuvem}/>
-    </View>
+   <Background>
+    <ContainerBar>
+      <Image source={require('../../assets/images/menu.png')}/>
+      <Nome>Bem vindo Henrique</Nome>
+      <Image source={require('../../assets/images/perfil.png')}/>
+    </ContainerBar>
 
-    <View style={estilo.faixa}>
-       <Image source={require('../../assets/images/quadro.png')}/>
-    </View>
+    <BotaoBanner onPress={() => navigation.navigate('Agendamento')}>
+    <Banner source={require('../../assets/images/Banner.png')}/>
+    </BotaoBanner>
 
-    <View style={estilo.containerTexto1}>
-    <Text style={{fontSize: 18}}>Bem-vindo ao</Text>
-    <Text style={{fontWeight: 'bold', color: '#22215B', fontSize: 16}}>Sistema de Atendimento ao Usuário</Text>
-    </View>
+    <Search>
+    <Pesquisa
+    placeholder='Procurar Serviço'
+    autoCorrect={false}
+    underlineColorAndroid="transparent"
+    />
+    <Lupa source={require('../../assets/images/Search.png')} style={{resizeMode:'stretch'}}/>
+    </Search>
 
-    <View style={estilo.containerTexto1}>
-    <Text style={{width: 280, fontSize: 15}}>
-        Governo do estado em parceria com equipe de 
-        desenvolvedores universitários juntos para atender a 
-        comunidade com mais eficiência e rapidez. Tudo isso sem 
-        precisar sair de Casa !
-    </Text>
-    </View>
+    <Servico>
+    <STitulo>Outros Serviços</STitulo>
+    <SubTitulo>Ver mais</SubTitulo>
+    </Servico>
 
-    <TouchableOpacity style={estilo.botao}>
-        <Text style={{color: '#FFF', fontWeight: 'bold'}}>Entrar</Text>
-    </TouchableOpacity>
+    <ContainerCard>
+      <Card style={{elevation: 5}}>
+      <Image source={require('../../assets/images/Pill.png')}/>
+      </Card>
 
-    <Image source={require('../../assets/images/detalhe3.png')} style={estilo.detalhe3}/>
-    <Image source={require('../../assets/images/detalhe2.png')} style={estilo.detalhe2}/>
-   
-   </View>
+      <Card style={{elevation: 5}}>
+      <Image source={require('../../assets/images/Syringe.png')}/>
+      </Card>
 
-   
+      <Card style={{elevation: 5}}>
+      <Image source={require('../../assets/images/Tooth.png')}/>
+      </Card>
+
+      <Card style={{elevation: 5}}>
+      <Image source={require('../../assets/images/Heartbeat.png')}/>
+      </Card>
+      
+    </ContainerCard>
+    <RowTitulo>
+      <TituloCard>Medicamentos</TituloCard>
+      <TituloCard>Vacinas</TituloCard>
+      <TituloCard>Dentista</TituloCard>
+      <TituloCard>Cardiologista</TituloCard>
+    </RowTitulo>
+
+    <Servico>
+    <STitulo>Outros Pacotes</STitulo>
+    <SubTitulo>Ver mais</SubTitulo>
+    </Servico>
+
+    <ContainerCard>
+      <Image source={require('../../assets/images/Pacote1.png')}/>
+      <Image source={require('../../assets/images/Pacote2.png')}/>
+    </ContainerCard>
+
+   </Background>
   );
 }
-
-
-const estilo = StyleSheet.create({
-    texto:{
-        fontFamily: 'Roboto.Bold',
-    },
-    container:{
-        flex: 1,
-        backgroundColor: '#FFF',
-        flexDirection: 'column'
-    },
-    nuvem:{
-        marginEnd: 30,
-        marginStart: 40
-        
-    },
-    containerRow:{
-       flexDirection: 'row',
-       justifyContent: 'space-between'
-    },
-    faixa:{
-        width: 360,
-        bottom: 12,
-        height: 180,
-        backgroundColor: '#68C0AB',
-        elevation: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    containerTexto1:{
-        textAlign: 'center',
-        marginTop : 25,
-        marginLeft: 40
-    },
-    botao:{
-        width: 280,
-        height: 40,
-        borderRadius: 7,
-        top: 10,
-        backgroundColor: '#68C0AB',
-        justifyContent: 'center',
-        alignItems: 'center',
-        left: 35
-    },
-    detalhe2:{
-        position: 'relative',
-        bottom: "18%",
-        zIndex: 0,
-    },
-    detalhe3:{
-        top:25,
-        zIndex: 1,
-    }
-
-    
-})
